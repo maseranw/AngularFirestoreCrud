68dc797d86866fe77cd61518ce153325ffdd027a
maserangelekanyo <witness.masera@eoh.co.za>
2019-05-30 13:29:57 +0200

    fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
     authDomain: "",
     databaseURL: "",
     projectId: "",
-    storageBucket: "m",
+    storageBucket: "",
     messagingSenderId: "",
     appId: ""
   }
